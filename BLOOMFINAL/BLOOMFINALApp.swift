5b0258dacfe6cce2fe95d69e1c130c9794c653ab
Sitanshu Pokalwar <sitz2309@gmal.com>
2023-06-10 23:11:32 +0530

    Initial Commit

---
+++ BLOOMFINAL/BLOOMFINALApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLOOMFINALApp.swift
+//  BLOOMFINAL
+//
+//  Created by Sitanshu Pokalwar on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BLOOMFINALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
