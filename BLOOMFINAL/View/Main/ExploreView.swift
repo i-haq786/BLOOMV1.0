03f61e8b69582e2c9faf86c243f998db79e83ecd
Sitanshu Pokalwar <sitz2309@gmal.com>
2023-06-15 14:45:12 +0530

    carousel for tags

--- BLOOMFINAL/View/Main/ExploreView.swift
+++ BLOOMFINAL/View/Main/ExploreView.swift
@@ -10,18 +10,50 @@ import SwiftUI
 struct ExploreView: View {
     
     @State private var recentEvents: [Event] = []
+    @State private var activeTag: String = "Pottery"
     
     var body: some View {
         NavigationStack{
-            ScrollView(.vertical, showsIndicators: false){
-                EventContentView(events: $recentEvents)
+            VStack{
+                TagView()
+                ScrollView(.vertical, showsIndicators: false){
+                    EventContentView(events: $recentEvents)
+                }
+            }
+            .navigationTitle("Events")
                 
-            }.navigationTitle("Events")
-                .padding()
-                .background(Color("background").edgesIgnoringSafeArea(.all))
         }
     }
+    
+    @ViewBuilder
+    func TagView() -> some View{
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 10) {
+                ForEach(tags, id: \.self) { tag in
+                    Text (tag)
+                        .font(.caption)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 6)
+                        .background{
+                            if activeTag == tag {
+                            Capsule ()
+                                .fill(Color("tab"))
+                               
+                            } else {
+                            Capsule ()
+                             .fill(.gray.opacity(0.2))
+                            }
+                        }
+                        .foregroundColor (activeTag == tag ? Color("otab") : Color("tab"))
+                }
+            } .padding(.horizontal, 8)
+        }
+    }
+    
 }
+var tags: [String] = [
+    "Coding", "Pottery", "Gardening", "Cooking", "Wellness", "Exercise", "Dance & Music"
+]
 
 struct ExploreView_Previews: PreviewProvider {
     static var previews: some View {
